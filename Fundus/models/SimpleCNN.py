e127eaced0baaed6a33fdd6dd12dac74e1af0e5f
coxjoseph <coxjoseph4004@gmail.com>
2024-06-04 16:21:22 -0400

    initial code for simple CNN

---
+++ Fundus/models/SimpleCNN.py
@@ -0,0 +1,23 @@
+from torch import nn
+import torch
+
+class SimpleCNN(nn.Module):
+    def __init__(self):
+        super().__init__()
+        self.conv1 = nn.Conv2d(3, 6, 5)  # 3 input channels (RGB), 6 output channels, 5x5 square convolution
+        self.pool = nn.MaxPool2d(2, 2)
+        self.conv2 = nn.Conv2d(6, 16, 5)
+        self.fc1 = nn.Linear(16 * 61 * 61, 120)
+        self.fc2 = nn.Linear(120, 84)
+        self.fc3 = nn.Linear(84, 1)
+        self.sigmoid = nn.Sigmoid()
+
+    def forward(self, x):
+        x = self.pool(nn.functional.relu(self.conv1(x)))
+        x = self.pool(nn.functional.relu(self.conv2(x)))
+        x = x.view(-1, 16 * 61 * 61)  # Flatten
+        x = nn.functional.relu(self.fc1(x))
+        x = nn.functional.relu(self.fc2(x))
+        x = self.fc3(x)
+        x = self.sigmoid(x)
+        return torch.squeeze(x)
