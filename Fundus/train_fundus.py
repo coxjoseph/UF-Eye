0968eab8aa18aa902e2b234c1982dd4d6b17b7b5
coxjoseph <coxjoseph4004@gmail.com>
2024-09-25 15:40:05 -0400

    bigger learning rate

--- Fundus/train_fundus.py
+++ Fundus/train_fundus.py
@@ -115,7 +115,7 @@ def train_fold(fold_index: int, json_path: Path, device: torch.device, batch_siz
 
     print(f'Device {device} training DeepCNN...')
     model = DeepCNN.DeepCNN().to(device)
-    optimizer = AdamW(model.parameters(), lr=0.001)
+    optimizer = AdamW(model.parameters(), lr=0.01)
     val_loss = train_model(model, optimizer, device, num_epochs, criterion, train_loader, val_loader,
                            model_id=f'DeepCNN-fold_{fold_index}')
 
